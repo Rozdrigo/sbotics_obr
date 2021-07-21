9fd50c8333f2079b443a703ec677ffbb0b87d888
Rodrigo Silva Coelho <rodrigocoelho236@gmail.com>
2021-07-21 15:00:50 -0300

    Formatando e organizando arquivos

--- Percurso.cs
+++ Percurso.cs
@@ -3,25 +3,25 @@ void Main()
     int passosNoVazio = 0;
     int[] linha = new int[105];
     
-    bool estaArena = false;
-    bool clive = false;
+    bool estouArena = false;
+    bool passRampa = false;
 
     while (true)
     {
         // CURVAS 90°
-        Func<int, int, int, int> encLinha = (direitaE, rotacoes, verde90) =>
+        Func<int, int, int, int> girarParaLinha = (direcao, rotacao, verde) =>
         {
-            bc.MoveFrontalRotations(300, rotacoes);
+            bc.MoveFrontalRotations(300, rotacao);
             bc.Wait(500);
-            bc.MoveFrontalAngles(500, 45 * verde90 * direitaE);
+            bc.MoveFrontalAngles(500, 45 * verde * direcao);
             while (bc.ReturnColor(2) != "PRETO")
             {
-                bc.MoveFrontal(-1000 * direitaE, 1000 * direitaE);
+                bc.MoveFrontal(-1000 * direcao, 1000 * direcao);
             }
             return 0;
         };
 
-        if (estaArena)
+        if (estouArena)
         {
             bc.MoveFrontalAngles(300, 90);
 
@@ -29,12 +29,18 @@ void Main()
             {
                 bc.MoveFrontalRotations(-300, 8);
             }
-            bc.MoveFrontalRotations(300, 0.5f);
-            bc.MoveFrontalAngles(300, -90);
-
             int cm = 0;
             while (true)
             {
+                if(bc.Compass() < 92 && bc.Compass() > 88){
+                if(bc.Compass() > 90){
+                    bc.MoveFrontalAngles(300, -0.01f);
+                }else if(bc.Compass() < 90){
+                    bc.MoveFrontalAngles(300, 0.01f);
+                };
+                };
+                bc.MoveFrontalRotations(300, 0.2f);
+                bc.MoveFrontalRotations(300, -90);
                 if (!bc.DetectDistance(0, 0, 1))
                 {
                     cm++;
@@ -61,9 +67,25 @@ void Main()
                 if (bc.ReturnColor(2) == "PRETO" && !bc.DetectDistance(2, 20, 25))
                 {
                     bc.MoveFrontal(170, 170);
-                } // AJUSTE
-                else if (bc.ReturnColor(1) == "PRETO" || bc.ReturnColor(0) == "PRETO")
+                }
+                 // FORA DE LINHA [PARADO]
+                else if ( bc.Lightness(0) > 40 && bc.Lightness(1) > 40 && bc.Lightness(2) > 40 && bc.Lightness(3) > 40 && bc.Lightness(4) > 40 && bc.Inclination() < 15)
                 {
+                    bc.MoveFrontalRotations(170, 1);
+                    passosNoVazio++;
+                    if (passosNoVazio > 20)
+                    {
+                        passosNoVazio = 0;
+                        bc.Wait(200);
+                        bc.MoveFrontalRotations(-170, 30);
+                    }
+                }
+                else{
+                    bc.MoveFrontal(170, 170);
+                }
+                //AJUSTE
+                if (bc.ReturnColor(1) == "PRETO" || bc.ReturnColor(0) == "PRETO")
+                { 
                     while (bc.ReturnColor(2) != "PRETO")
                     {
                         bc.MoveFrontal(-1000, 1000);
@@ -75,17 +97,7 @@ void Main()
                     {
                         bc.MoveFrontal(1000, -1000);
                     }
-                } // FORA DE LINHA [PARADO]
-                else
-                {
-                    bc.MoveFrontalRotations(180, 1);
-                    passosNoVazio++;
-                    if (passosNoVazio > 14)
-                    {
-                        passosNoVazio = 0;
-                        bc.MoveFrontalRotations(-180, 20);
-                    }
-                }
+                };
             } //DESVIAR DE OBSTACULOS
             else if (bc.DetectDistance(2, 20, 25))
             { //RAMPA
@@ -99,7 +111,7 @@ void Main()
                     bc.MoveFrontalAngles(300, 45);
                     while (!bc.DetectDistance(1, 0, 30))
                     {
-                        bc.MoveFrontal(180, 180);
+                        bc.MoveFrontal(170, 170);
                     };
                     bc.MoveFrontalRotations(500, 18);
                     bc.MoveFrontalAngles(500, 45);
@@ -109,37 +121,32 @@ void Main()
             } //MARCAÇÕES VERDES DIREITA
             else if (bc.ReturnColor(1) == "VERDE" && bc.ReturnColor(3) == "BRANCO")
             {
-                encLinha(1, 15, 1);
+                girarParaLinha(1, 15, 1);
             } //MARCAÇÕES VERDES ESQUERDA
             else if (bc.ReturnColor(3) == "VERDE" && bc.ReturnColor(1) == "BRANCO")
             {
-                encLinha(-1, 15, 1);
+                girarParaLinha(-1, 15, 1);
             } // CURVA DE 90 LINHA PRETA DIREITA
             else if (bc.ReturnColor(0) == "PRETO" && bc.ReturnColor(1) == "PRETO" && bc.ReturnColor(3) == "BRANCO" && bc.ReturnColor(4) == "BRANCO")
             {
-                encLinha(1, 14, 0);
+                girarParaLinha(1, 14, 0);
             } // CURVA DE 90 LINHA PRETA ESQUERDA
             else if (bc.ReturnColor(0) == "BRANCO" && bc.ReturnColor(1) == "BRANCO" && bc.ReturnColor(3) == "PRETO" && bc.ReturnColor(4) == "PRETO")
             {
-                encLinha(-1, 14, 0);
-            }
-            //ANDANDO SEM LINHA
-            else if (bc.ReturnColor(0) == "BRANCO" && bc.ReturnColor(1) == "BRANCO" && bc.ReturnColor(2) == "BRANCO" && bc.ReturnColor(3) == "BRANCO" && bc.ReturnColor(4) == "BRANCO")
-            {
-                count++;
-                bc.Print(count);
-            }
+                girarParaLinha(-1, 14, 0);
+            };
+
             // SUBINDO A RAMPA
             if (bc.Inclination() > 330 && bc.DetectDistance(1, 30, 40))
             {
-                clive = true;
+                passRampa = true;
             }
             // ANALIZANDO ESTADO ANTERIOR DO CARRINHO
-            else if (clive && bc.Inclination() == 0)
+            else if (passRampa && bc.Inclination() == 0)
             {
-                estaArena = true;
+                estouArena = true;
             }
         };
-        bc.Print(" ESTOU NA ARENA: " + estaArena + " PASSEI NA RAMPA: " + clive);
+        bc.Print(" ESTOU NA ARENA: " + estouArena + " PASSEI NA RAMPA: " + passRampa);
     }
 }
